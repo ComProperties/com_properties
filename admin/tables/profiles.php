74d9c784ece23006d961f2e8ee40da8de3ac4cbd
ComProperties <fabiouz@hotmail.com>
2016-06-09 09:08:58 -0300

    Update profiles.php

--- admin/tables/profiles.php
+++ admin/tables/profiles.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package		Joomla.Administrator
+ * @subpackage	com_properties
+ * @copyright	Copyright (C) 2006 - 2016 Fabio Esteban Uzeltinger.
+ * @email		fabiouz@gmail.com
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined( '_JEXEC' ) or die( 'Restricted access' );
 class PropertiesTableprofiles extends JTable
 {    
