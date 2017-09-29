640bd774b3ee1800928b2e56b8c3d0dfc91b7d31
ComProperties <fabiouz@hotmail.com>
2017-09-29 19:33:23 -0300

    Add files via upload

--- admin/views/product/view.html.php
+++ admin/views/product/view.html.php
@@ -33,19 +33,18 @@ class PropertiesViewProduct extends JViewLegacy
 		$canAddProperties=$params->get('canAddProperties',5);
 		$canAddImages=$params->get('canAddImages',5);
 
-/**/
-if(!isset($this->Profile))
-	{
-	$this->Profile = new JObject();
-if(!isset($this->Profile->canaddproperties))
-	{
-	$this->Profile->canaddproperties=$canAddProperties;
-	}
-if(!isset($this->Profile->canaddimages))
-	{
-	$this->Profile->canaddimages=$canAddImages;
-	}	
-}
+		if(!isset($this->Profile))
+			{
+			$this->Profile = new JObject();
+		if(!isset($this->Profile->canaddproperties))
+			{
+			$this->Profile->canaddproperties=$canAddProperties;
+			}
+		if(!isset($this->Profile->canaddimages))
+			{
+			$this->Profile->canaddimages=$canAddImages;
+			}	
+		}
 		// Check for errors.
 		if (count($errors = $this->get('Errors'))) {
 			JFactory::getApplication()->enqueueMessage(implode("\n", $errors), 'error');
@@ -62,128 +61,23 @@ if(!isset($this->Profile->canaddimages))
 		$Images=$this->getImages($this->item->id);
 		$this->assignRef('Images',		$Images);
 		
-$document = JFactory::getDocument();
+		$document = JFactory::getDocument();
+		$document->addScript("http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js");  
+		$session = JFactory::getSession();
 
-/*  */ 
- //$document->addScript("http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
-//$document->addScript("http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js");  
-
-
-$document->addScript("http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js");  
-
-/*
-$document->addScript("components/com_properties/includes/js/vendor/jquery.ui.widget.js");
-$document->addScript("components/com_properties/includes/js/jquery.iframe-transport.js");
-$document->addScript("components/com_properties/includes/js/jquery.fileupload.js");
-*/
-
-$session = JFactory::getSession();
-
-//$myItemObject->id=62;
-/*
-thumbnail_url: "'.JURI::root().'images/properties/images/",
-flash_url : "'.JURI::base().'components/com_properties/includes/SWFUpload/swfupload/swfupload.swf",
-"idproduct" : "'.$this->item->id.'",	
-"option" : "com_properties",
-				"view" : "images",                
-				"format" : "raw",
-                "task" : "images.swfupload_files",		
-*/				
-				
-
-/*	
-$document->addScriptDeclaration("
-	jQuery(function () {
-    
-    // Change this to the location of your server-side upload handler:
-    var url = 'index.php?option=com_properties&task=images.save_images&format=raw&id=1'; 
-	//JURI::root().'images/properties/images/\';
-    jQuery('#fileupload').fileupload({
-        url: url,
-        dataType: 'json',
-        done: function (e, data) {
-            jQuery.each(data.result.files, function (index, file) {
-                jQuery('<p/>').text(file.name).appendTo('#files');
-            });
-        },
-        progressall: function (e, data) {
-            var progress = parseInt(data.loaded / data.total * 100, 10);
-            jQuery('#progress .progress-bar').css(
-                'width',
-                progress + '%'
-            );
-        }
-    }).prop('disabled', !jQuery.support.fileInput)
-        .parent().addClass(jQuery.support.fileInput ? undefined : 'disabled');
-});   
-");
-*/
-
-if($this->params->get('categoryToShowCalendar','-1') == $this->item->cid)
-						{
-
-/*
-$doc->addScriptDeclaration('
-window.addEvent(\'domready\', function() {   
-elementoFormulario = $("calendar-form");    
-   elementoFormulario.addEvent(\'submit\', function(e) {	  
-      e.stop();	  
-	  $("progressCalendar").setStyle("visibility","visible");	  
-      this.set(\'send\', {		 
-         onComplete: function(respuesta) {		 			
-            $("resultCalendar").set(\'html\', respuesta);	
-			$("progressCalendar").setStyle("visibility","hidden");		
-         }
-      });
-      this.send();
-   });
-});
-');
-
-$doc->addScriptDeclaration('
-window.addEvent(\'domready\', function() {   
-elementoFormulario = $("rates-form");    
-   elementoFormulario.addEvent(\'submit\', function(e) {	  
-      e.stop();	  
-	  $("progressRRL").setStyle("visibility","visible");	  
-      this.set(\'send\', {		 
-         onComplete: function(respuesta) {		 			
-            $("RefreshRatesList").set(\'html\', respuesta);	
-			$("progressRRL").setStyle("visibility","hidden");		
-         }
-      });
-      this.send();
-   });
-});
-');
-
-
-$doc->addScriptDeclaration('
-window.addEvent(\'domready\', function() {   
-	$$(\'.publishstopme\').each(function(el) {
-    	el.addEvent(\'click\', function(e) {
-      	e = new Event(e).stop();
-		});
-	});
-});
-');
-*/
-			}
-
-
-
-	}
+		}
 	
 	
 	
 	
-	$canDo	= PropertiesHelper::getActions();
-	if ($canDo->get('core.manage')) {
+		$canDo	= PropertiesHelper::getActions();
+		if ($canDo->get('core.manage')) {
 		$this->iAmAdmin=true;
 		}else{
 		$this->iAmAdmin=false;
 		$this->checkAgent();
 		}
+		JHTML::_('behavior.modal'); 	
 			
 		parent::display($tpl);
 	}
